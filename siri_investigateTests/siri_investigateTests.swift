a10ecaa584b4b300437cc76b6cb2eb2b139f8f7d
Aliaksei Horbel <gorbel.allex@gmail.com>
2021-02-28 19:05:38 +0300

    Initial Commit

---
+++ siri_investigateTests/siri_investigateTests.swift
@@ -0,0 +1,33 @@
+//
+//  siri_investigateTests.swift
+//  siri_investigateTests
+//
+//  Created by Alexey Gorbel on 28.02.21.
+//
+
+import XCTest
+@testable import siri_investigate
+
+class siri_investigateTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
