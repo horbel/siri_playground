b4de4cd8955b50c8eab9597f922a469874aaa702
Aliaksei Horbel <gorbel.allex@gmail.com>
2021-03-01 21:08:14 +0300

    add highlight for voiced text

--- siri_investigate/ViewController.swift
+++ siri_investigate/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVSpeechSynthesizerDelegate {
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func readButton(_ sender: Any) {
+        voiceText()
+    }
+    
+    let synthesizer = AVSpeechSynthesizer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        synthesizer.delegate = self;
     }
-
-
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, willSpeakRangeOfSpeechString characterRange: NSRange, utterance: AVSpeechUtterance) {
+        let mutableAttributedString = NSMutableAttributedString(attributedString: utterance.attributedSpeechString)
+        mutableAttributedString.addAttribute(.foregroundColor, value: UIColor.red, range: characterRange)
+        textView.attributedText = mutableAttributedString
+    }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        textView.attributedText = NSAttributedString(attributedString: utterance.attributedSpeechString)
+    }
+    
+    func voiceText(){
+        let string = textView.text!
+        let utterance = AVSpeechUtterance(attributedString: textView.attributedText)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        
+        synthesizer.delegate = self
+        synthesizer.speak(utterance)
+    }
 }
 
